707a9ba2bb7191ee0cf150596e6302a580b37f59
Tara-1022 <taraann@karunya.edu.in>
2023-10-03 22:13:16 +0530

    Minor changes: bug allowing toggled cells to stay alive. Link commented code

--- board_functions.js
+++ board_functions.js
@@ -1,7 +1,5 @@
-// import {board_render_size, alive_color, dead_color} from 'constants.json'
-const board_render_size = 50;
-const alive_color = "yellow";
-const dead_color = "black";
+import { evolve } from "./game_logic.js";
+import { alive_color, dead_color, board_render_size } from "./constants.js";
 // row id -> row_<ind>
 // cell id -> cell_<row>.<col>
 
@@ -24,36 +22,57 @@ function generateBoard(n) {
                 "' class='cell' style='background-color:" +
                 dead_color +
                 ";'>" +
-                // cell_id +
                 "</div>")
         }
     }
 }
 
-function to_cell_id(cell) {
+export function to_cell_id(cell) {
     return "cell_" + cell[0] + "." + cell[1]
 }
 
-function to_cell(cell_id) {
-    return cell_id.slice(4).split(".").map(parseInt)
+export function to_cell(cell_id) {
+    return cell_id.slice(5).split(".").map((n) => parseInt(n))
+}
+
+function is_renderable_cell(cell_id){
+    let cell = to_cell(cell_id)
+    return (cell[0] < board_render_size) && (cell[1] < board_render_size)
 }
 
 function set_cells(cells, color) {
     let cell_element;
-    for (cell_id of cells) {
+    for (let cell_id of cells) {
+        if (!is_renderable_cell(cell_id)) {
+            continue;
+        }
         cell_element = document.getElementById(cell_id)
         cell_element.style.backgroundColor = color
     }
 }
 
 function set_cell(cell_id, color) {
+    if (!is_renderable_cell(cell_id)) {
+        return;
+    }
     let cell_element = document.getElementById(cell_id)
     cell_element.style.backgroundColor = color
 }
 
 function toggle_cell(cell_id) {
+    if (!is_renderable_cell(cell_id)) {
+        return;
+    }
     let cell_element = document.getElementById(cell_id)
     cell_element.style.backgroundColor = (cell_element.style.backgroundColor == alive_color) ? dead_color : alive_color
+}
+
+function nextState() {
+    let cell;
+    let now_alive_cells = evolve(alive_cells)
+    set_cells(alive_cells, dead_color)
+    set_cells(now_alive_cells, alive_color)
+    alive_cells = now_alive_cells
 }
 
 
@@ -62,7 +81,8 @@ jQuery(window).on('load', function () {
 
     jQuery(".cell").click(function () {
         if (edit_allowed) {
-            alive_cells.add(to_cell(jQuery(this).attr("id")))
+            let cell_id = jQuery(this).attr("id")
+            alive_cells.has(cell_id)? alive_cells.delete(cell_id) : alive_cells.add(cell_id)
             toggle_cell(jQuery(this).attr("id"))
         }
     })
@@ -77,9 +97,14 @@ jQuery(window).on('load', function () {
 
     jQuery(".cell").mouseenter(function () {
         if (edit_allowed && drag_state_on) {
-            alive_cells.add(to_cell(jQuery(this).attr("id")))
+            let cell_id = jQuery(this).attr("id")
+            alive_cells.has(cell_id)? alive_cells.delete(cell_id) : alive_cells.add(cell_id)
             toggle_cell(jQuery(this).attr("id"))
         }
+    })
+
+    jQuery("#evolve").click(function () {
+        nextState()
     })
 
 })
--- game_logic.js
+++ game_logic.js
@@ -1,26 +1,32 @@
-import {board_render_size} from 'constants.json'
+import {alive_color, board_render_size, offsets} from './constants.js'
+import {to_cell_id, to_cell} from './board_functions.js'
 
-const offsets = [[-1,0], [1,0], [0,1], [0,-1], [-1, -1], [1, 1], [-1, 1], [1, -1]]
 const board_size = board_render_size + 3;
 
-function will_live(cell){
+function will_live(cell_id, now_alive){
     let alive_neighbours = 0;
     let neighbour;
-    for(offset of offsets){
+    let cell= to_cell(cell_id)
+    for(let offset of offsets){
         neighbour = [cell[0]+ offset[0], cell[1] + offset[1]]
-        if(alive_cells.has(neighbour)){
+        if(now_alive.has(to_cell_id(neighbour))){
             alive_neighbours += 1
         }
     }
-    return alive_neighbours == 3
+    if(now_alive.has(cell_id)){
+        return alive_neighbours == 2 || alive_neighbours == 3
+    }
+    else{
+        return alive_neighbours == 3
+    }
 }
 
-function evolve(){
+export function evolve(now_alive){
     let next_alive = new Set()
     for(let row=0; row < board_size; row+=1){
         for(let col=0; col < board_size; col+=1){
-            if(will_live([row, col])){
-                next_alive.add([row, col])
+            if(will_live(to_cell_id([row, col]), now_alive)){
+                next_alive.add(to_cell_id([row, col]))
             }
         }
     }
