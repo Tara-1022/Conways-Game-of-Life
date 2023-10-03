5eecef9e81ece8996ee9ce5fef7e6bb708a42c38
Tara-1022 <taraann@karunya.edu.in>
2023-10-03 22:07:13 +0530

    Complete game logic & basic interface

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const dead_color = "black";
+export const alive_color = "yellow";
+export const board_render_size = 50;
+export const offsets = [[-1,0], [1,0], [0,1], [0,-1], [-1, -1], [1, 1], [-1, 1], [1, -1]];
