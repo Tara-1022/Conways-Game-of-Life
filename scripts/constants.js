d46935b097be815e49039588b7233f90952e4052
Tara-1022 <taraann@karunya.edu.in>
2023-11-19 11:26:22 +0530

    Update overflow amount

--- scripts/constants.js
+++ scripts/constants.js
@@ -4,6 +4,6 @@ const board_render_size = 50;
 const offsets = [[-1,0], [1,0], [0,1], [0,-1], [-1, -1], [1, 1], [-1, 1], [1, -1]];
 
 // some invisible rows and columns on each end to give the impression of an infinite board
-const overflow_amt = 3;
+const overflow_amt = 15;
 
 export {dead_color, alive_color, board_render_size, overflow_amt, offsets}
